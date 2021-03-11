0e33ebd33c1e1e309b783c9e5b88a1d60822ad3d
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-03-11 20:47:08 +0200

    init first image

--- app/src/main/java/com/asennikolaev/learninganimals/QuizAnimalActivity.java
+++ app/src/main/java/com/asennikolaev/learninganimals/QuizAnimalActivity.java
@@ -2,10 +2,14 @@ package com.asennikolaev.learninganimals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class QuizAnimalActivity extends AppCompatActivity {
+
+    private ImageView imageViewQuestion;
 
     private Button buttonAnswer1;
     private Button buttonAnswer2;
@@ -18,13 +22,20 @@ public class QuizAnimalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_animal);
 
-        initQuizAnswerButtons();
+
+        initQuizScreenComponents();
     }
 
-    private void initQuizAnswerButtons() {
+    private void initQuizScreenComponents() {
+
+        imageViewQuestion = (ImageView) findViewById(R.id.imageView);
         buttonAnswer1 = (Button) findViewById(R.id.buttonAnswer1);
         buttonAnswer2 = (Button) findViewById(R.id.buttonAnswer2);
         buttonAnswer3 = (Button) findViewById(R.id.buttonAnswer3);
         buttonAnswer4 = (Button) findViewById(R.id.buttonAnswer4);
+
+        imageViewQuestion.setImageResource(R.drawable.bunny);//set image
+
+        System.out.println("initQuizScreenComponents completed");
     }
 }
