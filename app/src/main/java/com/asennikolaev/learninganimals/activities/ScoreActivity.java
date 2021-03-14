af218a780061a4162dd6fc5096512fff73a14dd0
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-03-14 19:20:49 +0200

    display correct/incorrect score

--- app/src/main/java/com/asennikolaev/learninganimals/activities/ScoreActivity.java
+++ app/src/main/java/com/asennikolaev/learninganimals/activities/ScoreActivity.java
@@ -2,6 +2,8 @@ package com.asennikolaev.learninganimals.activities;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,13 +12,33 @@ import com.asennikolaev.learninganimals.score.QuizScore;
 
 public class ScoreActivity  extends AppCompatActivity {
 
+    private TextView correctAnswersCountTextView;
+    private TextView incorrectAnswersCountTextView;
+
+    private QuizScore quizScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        quizScore = (QuizScore) getIntent().getSerializableExtra("QuizScore");
+
+        Log.i("TAG","score is " + quizScore);
+
         setContentView(R.layout.activity_score);
 
-       QuizScore quizScore = (QuizScore) getIntent().getSerializableExtra("QuizScore");
-        Log.i("TAG","score is " + quizScore);
+        initQuizScreenComponents();
+
+
+    }
+
+    private void initQuizScreenComponents() {
+
+        correctAnswersCountTextView = (TextView) findViewById(R.id.correctAnswersCountTextView);
+        incorrectAnswersCountTextView = (TextView) findViewById(R.id.incorrectAnswersCountTextView);
+
+        correctAnswersCountTextView.setText("Correct answers: " + quizScore.getCorrectAnswersCount());
+        incorrectAnswersCountTextView.setText("Incorrect answers: " + quizScore.getIncorrectAnswersCount());
 
     }
 }
