b8b2ad08da66a7511dea5e5aad7ae497fcd5028e
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-03-15 19:08:52 +0200

    score recycler view init

--- app/src/main/java/com/asennikolaev/learninganimals/activities/ScoreActivity.java
+++ app/src/main/java/com/asennikolaev/learninganimals/activities/ScoreActivity.java
@@ -6,11 +6,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.asennikolaev.learninganimals.R;
 import com.asennikolaev.learninganimals.score.QuizScore;
+import com.asennikolaev.learninganimals.score.ScoreRecyclerViewAdapter;
 
 public class ScoreActivity  extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     private TextView correctAnswersCountTextView;
     private TextView incorrectAnswersCountTextView;
@@ -34,11 +39,20 @@ public class ScoreActivity  extends AppCompatActivity {
 
     private void initQuizScreenComponents() {
 
-        correctAnswersCountTextView = (TextView) findViewById(R.id.correctAnswersCountTextView);
-        incorrectAnswersCountTextView = (TextView) findViewById(R.id.incorrectAnswersCountTextView);
+//        correctAnswersCountTextView = (TextView) findViewById(R.id.correctAnswersCountTextView);
+////        incorrectAnswersCountTextView = (TextView) findViewById(R.id.incorrectAnswersCountTextView);
 
-        correctAnswersCountTextView.setText("Correct answers: " + quizScore.getCorrectAnswersCount());
-        incorrectAnswersCountTextView.setText("Incorrect answers: " + quizScore.getIncorrectAnswersCount());
+
+//        correctAnswersCountTextView.setText("Correct answers: " + quizScore.getCorrectAnswersCount());
+//        incorrectAnswersCountTextView.setText("Incorrect answers: " + quizScore.getIncorrectAnswersCount());
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        String st[] = {"test","test","test","test","test","test","test","test","test","test","test","test","test","test","test"};
+        ScoreRecyclerViewAdapter scoreRecyclerViewAdapter = new ScoreRecyclerViewAdapter(st);
+        recyclerView.setAdapter(scoreRecyclerViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
