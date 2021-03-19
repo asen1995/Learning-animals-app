2392d5a7fbd1d5366959b87061836487ff2f438a
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-03-19 16:37:45 +0200

    adding more questions

--- app/src/main/java/com/asennikolaev/learninganimals/model/ImageResources.java
+++ app/src/main/java/com/asennikolaev/learninganimals/model/ImageResources.java
@@ -7,9 +7,11 @@ import java.util.Map;
 
 public class ImageResources {
 
-    protected static Map<Integer,String> imageAnswerMap = new HashMap<>();
+    protected static Map<Integer,String> imageAnswerMap;
 
     public static void initImages() {
+
+        imageAnswerMap  = new HashMap<>();
 
         imageAnswerMap.put(R.drawable.bunny, "bunny");
         imageAnswerMap.put(R.drawable.deer, "deer");
@@ -18,6 +20,60 @@ public class ImageResources {
         imageAnswerMap.put(R.drawable.camel, "camel");
         imageAnswerMap.put(R.drawable.shark, "shark");
         imageAnswerMap.put(R.drawable.frog, "frog");
+        imageAnswerMap.put(R.drawable.dolphin, "dolphin");
+        imageAnswerMap.put(R.drawable.beaver, "beaver");
+        imageAnswerMap.put(R.drawable.hedgehog, "hedgehog");
+        imageAnswerMap.put(R.drawable.cat, "cat");
+        imageAnswerMap.put(R.drawable.dog, "dog");
+        imageAnswerMap.put(R.drawable.iguana, "iguana");
+        imageAnswerMap.put(R.drawable.walrus, "walrus");
+        imageAnswerMap.put(R.drawable.owl, "owl");
+        imageAnswerMap.put(R.drawable.bee, "bee");
+        imageAnswerMap.put(R.drawable.ant, "ant");
+        imageAnswerMap.put(R.drawable.butterfly, "butterfly");
+        imageAnswerMap.put(R.drawable.kangaroo, "kangaroo");
+        imageAnswerMap.put(R.drawable.elephant, "elephant");
+        imageAnswerMap.put(R.drawable.bear, "bear");
+        imageAnswerMap.put(R.drawable.penguin, "penguin");
+        imageAnswerMap.put(R.drawable.rhino, "rhino");
+        imageAnswerMap.put(R.drawable.lion, "lion");
+        imageAnswerMap.put(R.drawable.tiger, "tiger");
+        imageAnswerMap.put(R.drawable.crocodile, "crocodile");
+        imageAnswerMap.put(R.drawable.turtle, "turtle");
+        imageAnswerMap.put(R.drawable.chameleon, "chameleon");
+        imageAnswerMap.put(R.drawable.swan, "swan");
+        imageAnswerMap.put(R.drawable.pelican, "pelican");
+        imageAnswerMap.put(R.drawable.panda, "panda");
+        imageAnswerMap.put(R.drawable.ostrich, "ostrich");
+        imageAnswerMap.put(R.drawable.parrot, "parrot");
+        imageAnswerMap.put(R.drawable.horse, "horse");
+        imageAnswerMap.put(R.drawable.bison, "bison");
+        imageAnswerMap.put(R.drawable.eagle, "eagle");
+        imageAnswerMap.put(R.drawable.hyena, "hyena");
+        imageAnswerMap.put(R.drawable.badger, "badger");
+        imageAnswerMap.put(R.drawable.zebra, "zebra");
+        imageAnswerMap.put(R.drawable.giraffe, "giraffe");
+        imageAnswerMap.put(R.drawable.hippopotamus, "hippopotamus");
+        imageAnswerMap.put(R.drawable.chimpanzee, "chimpanzee");
+        imageAnswerMap.put(R.drawable.gorilla, "gorilla");
+        imageAnswerMap.put(R.drawable.mammoth, "mammoth");
+        imageAnswerMap.put(R.drawable.flamingo, "flamingo");
+        imageAnswerMap.put(R.drawable.koala, "koala");
+        imageAnswerMap.put(R.drawable.platypus, "platypus");
+        imageAnswerMap.put(R.drawable.python, "python");
+        imageAnswerMap.put(R.drawable.balloonfish, "balloon fish");
+        imageAnswerMap.put(R.drawable.bat, "bat");
+        imageAnswerMap.put(R.drawable.jellyfish, "jellyfish");
+        imageAnswerMap.put(R.drawable.woodpecker, "woodpecker");
+        imageAnswerMap.put(R.drawable.stork, "stork");
+        imageAnswerMap.put(R.drawable.cuckoo, "cuckoo");
+        imageAnswerMap.put(R.drawable.goose, "goose");
+        imageAnswerMap.put(R.drawable.sparrow, "sparrow");
+        imageAnswerMap.put(R.drawable.pheasant, "pheasant");
+        imageAnswerMap.put(R.drawable.lizard, "lizard");
+        imageAnswerMap.put(R.drawable.octopus, "octopus");
+        imageAnswerMap.put(R.drawable.anteater, "anteater");
+
 
     }
 }
